6488946ccc25ea768b5fa7ee6ca0d3f0108824ee
Achraf-haddar <achraf.haddar@supcom.tn>
2020-08-16 13:42:57 +0200

    train.py

--- train.py
+++ train.py
@@ -43,3 +43,54 @@ print('x_train shape:', x_train.shape)
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 
+# Convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+# Build our model
+model = Sequential()
+# Convolutional layer with rectified linear unit activation
+model.add(Conv2D(32, kernel_size=(3,3),
+                 activation='relu',
+                 input_shape=input_shape))
+
+# Again
+model.add(Conv2D(64, (3, 3), activation='relu'))
+# Choose the best features via pooling
+model.add(MaxPooling2D(pool_size=(2, 2)))
+# Randomly turn neurons on and off to improve convergence
+model.add(Dropout(0.25))
+# Flatten since too many dimensions, we only want a classification 
+model.add(Flatten())
+# Fully connected to get all relevant data
+model.add(Dense(128, activation='relu'))
+# one more dropout for convergence
+model.add(Dropout(0.5))
+# Output a softmax to squash the matrix into output probabilities
+model.add(Dense(num_classes, activation='softmax'))
+# Adaptive learning rate (adaDelta) is a popular form of gradient descent
+# Categorical since we have multiple classes (10)
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+# train
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+
+# How well did it do ?
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+# Save the model 
+# Serialize model to JSON  (to save the architecure of the model)
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+# Serialize weights to HDF5
+model.save_weights("model.h5")
+print("Saved model to disk")
